3219d08a20b58e843ea2ea8c2d25d8a8ef4fa9de
Alex Beregszaszi <alex@rtfs.hu>
2018-08-12 01:43:38 +0100

    Use HashMap for translations

--- wasm-cleanup/src/lib.rs
+++ wasm-cleanup/src/lib.rs
@@ -2,9 +2,22 @@ extern crate parity_wasm;
 #[macro_use]
 extern crate log;
 
+use std::collections::HashMap;
+use std::env;
+
 use parity_wasm::elements::*;
 
-use std::env;
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
+pub struct ImportPair {
+    pub module: String,
+    pub field: String
+}
+
+impl ImportPair {
+    fn new(module: &str, field: &str) -> ImportPair {
+        ImportPair { module: module.to_string(), field: field.to_string() }
+    }
+}
 
 // FIXME: There is no `Module::import_section_mut()`
 fn import_section_mut(module: &mut Module) -> Option<&mut ImportSection> {
@@ -14,11 +27,11 @@ fn import_section_mut(module: &mut Module) -> Option<&mut ImportSection> {
     None
 }
 
-pub fn rename_imports(module: &mut Module) {
+pub fn rename_imports(module: &mut Module, translations: HashMap<ImportPair, ImportPair>) {
     if let Some(section) = import_section_mut(module) {
         for entry in section.entries_mut().iter_mut() {
-            if entry.module() == "env" && entry.field() == "ethereum_return" {
-              *entry = ImportEntry::new("ethereum".to_string(), "finish".to_string(), *entry.external())
+            if let Some(replacement) = translations.get(&ImportPair::new(entry.module(), entry.field())) {
+                *entry = ImportEntry::new(replacement.module.clone(), replacement.field.clone(), *entry.external())
             }
         }
     }
@@ -54,11 +67,20 @@ pub fn cleanup() {
 #[cfg(test)]
 mod tests {
     use parity_wasm;
+    use std::collections::HashMap;
+    use super::ImportPair;
 
     #[test]
     fn smoke_test() {
         let mut module = parity_wasm::deserialize_file("src/test.wasm").expect("failed");
-        ::rename_imports(&mut module);
+        let trans: HashMap<ImportPair, ImportPair> =
+            [
+                (ImportPair::new("env", "ethereum_return"), ImportPair::new("ethereum", "finish")),
+                (ImportPair::new("env", "ethereum_getCallDataSize"), ImportPair::new("ethereum", "getCallDataSize")),
+                (ImportPair::new("env", "ethereum_callDataSize"), ImportPair::new("ethereum", "getCallDataSize")),
+                (ImportPair::new("env", "ethereum_callDataCopy"), ImportPair::new("ethereum", "callDataCopy"))
+            ].iter().cloned().collect();
+        ::rename_imports(&mut module, trans);
         parity_wasm::serialize_to_file("src/test-out.wasm", module).expect("failed");
     }
 }
