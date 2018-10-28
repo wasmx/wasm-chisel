e70fd8e428cd1f3c7309a2eee08ca4fa6cad3cde
Alex Beregszaszi <alex@rtfs.hu>
2018-10-28 22:07:59 +0100

    Add very basic CLI
    Does ewasm specific remapimports and trimexports.

---
+++ chisel/src/main.rs
@@ -0,0 +1,30 @@
+extern crate libchisel;
+extern crate parity_wasm;
+
+use std::env;
+
+use libchisel::*;
+
+pub fn main() {
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() != 3 {
+        println!("Usage: {} in.wasm out.wasm", args[0]);
+        return;
+    }
+
+    let code = std::fs::read(&args[1]).expect("Failed to open and read file");
+
+    let mut module = parity_wasm::deserialize_buffer(&code).expect("Failed to load module");
+
+    let trimexports = trimexports::TrimExports::with_preset("ewasm");
+    trimexports
+        .translate(&mut module)
+        .expect("Failed to trim exports");
+
+    let remapimports = remapimports::RemapImports::new();
+    remapimports
+        .translate(&mut module)
+        .expect("Failed to remap imports");
+
+    parity_wasm::serialize_to_file(&args[2], module).expect("Failed to write module");
+}
