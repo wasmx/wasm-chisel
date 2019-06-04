1de6fbcf009495dfb5d93dd2e5799944e4adc586
Alex Beregszaszi <alex@rtfs.hu>
2019-06-04 10:13:34 +0100

    Implement from trait for parity-wasm SerializationError

--- libchisel/src/utils.rs
+++ libchisel/src/utils.rs
@@ -1,5 +1,6 @@
 //! These are helpers to be used internally.
 
+use super::ModuleError;
 use parity_wasm::elements::{deserialize_buffer, serialize, Module};
 
 pub trait HasNamesSection {
@@ -33,6 +34,13 @@ impl SerializationHelpers for Module {
 
     fn to_vec(self) -> Vec<u8> {
         serialize::<Module>(self).expect("invalid Wasm module")
+    }
+}
+
+impl From<parity_wasm::SerializationError> for ModuleError {
+    fn from(a: parity_wasm::SerializationError) -> Self {
+        use std::error::Error;
+        ModuleError::Custom(a.description().to_string())
     }
 }
 
@@ -76,5 +84,46 @@ mod tests {
         .unwrap();
         let module = Module::from_slice(&input);
         assert_eq!(module.has_names_section(), false);
+    }
+
+    fn try_serialize(module: Module) -> Result<Vec<u8>, ModuleError> {
+        Ok(serialize::<Module>(module)?)
+    }
+
+    fn try_deserialize(input: &[u8]) -> Result<Module, ModuleError> {
+        Ok(deserialize_buffer::<Module>(&input)?)
+    }
+
+    #[test]
+    fn test_serialize_error() {
+        // The failure case.
+        let module = parity_wasm::builder::module()
+            .export()
+            .field("invalid")
+            .internal()
+            .func(15)
+            .build()
+            .build();
+        // Shouldn't this one fail due to the invalid function reference?
+        assert_eq!(try_serialize(module).is_ok(), true);
+
+        // The success case.
+        assert_eq!(try_serialize(Module::default()).is_ok(), true)
+    }
+
+    #[test]
+    fn test_deserialize_error() {
+        // The failure case.
+        assert_eq!(try_deserialize(&[0u8; 0]).is_ok(), false);
+
+        // The success case.
+        let input = FromHex::from_hex(
+            "0061736d01000000010401600000030201000405017001010105030100100619
+037f01418080c0000b7f00418080c0000b7f00418080c0000b072503066d656d
+6f727902000b5f5f686561705f6261736503010a5f5f646174615f656e640302
+0a040102000b",
+        )
+        .unwrap();
+        assert_eq!(try_deserialize(&input).is_ok(), true)
     }
 }
