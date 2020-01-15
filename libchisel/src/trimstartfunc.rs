b945c6f99a764ad5e0a3c12452f7c45c0f0b45ba
Alex Beregszaszi <alex@rtfs.hu>
2020-01-15 00:25:22 +0000

    Implement ModuleConfig in trimstartfunc

--- libchisel/src/trimstartfunc.rs
+++ libchisel/src/trimstartfunc.rs
@@ -1,6 +1,8 @@
+use std::collections::HashMap;
+
 use parity_wasm::elements::Module;
 
-use super::{ChiselModule, ModuleError, ModuleKind, ModulePreset, ModuleTranslator};
+use super::{ChiselModule, ModuleConfig, ModuleError, ModuleKind, ModulePreset, ModuleTranslator};
 
 pub struct TrimStartFunc;
 
@@ -28,6 +30,20 @@ impl<'a> ChiselModule<'a> for TrimStartFunc {
 
     fn as_abstract(&'a self) -> Self::ObjectReference {
         self as Self::ObjectReference
+    }
+}
+
+impl ModuleConfig for TrimStartFunc {
+    fn with_defaults() -> Result<Self, ModuleError> {
+        Err(ModuleError::NotSupported)
+    }
+
+    fn with_config(config: &HashMap<String, String>) -> Result<Self, ModuleError> {
+        if let Some(preset) = config.get("preset") {
+            TrimStartFunc::with_preset(preset)
+        } else {
+            Err(ModuleError::NotSupported)
+        }
     }
 }
 
